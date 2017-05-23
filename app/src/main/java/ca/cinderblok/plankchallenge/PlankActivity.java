d94040cf832b78c3e783debc2b19a3d190b1b444
RACHEL HARTVIKSEN <rhartviksen@gmail.com>
2017-05-22 23:28:42 -0400

    Add some description text and debug text

--- app/src/main/java/ca/cinderblok/plankchallenge/PlankActivity.java
+++ app/src/main/java/ca/cinderblok/plankchallenge/PlankActivity.java
@@ -1,13 +1,34 @@
 package ca.cinderblok.plankchallenge;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.net.Uri;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class PlankActivity extends AppCompatActivity {
+
+    private TextView debugText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plank);
+
+        debugText = (TextView) findViewById((R.id.debug_textview));
+        if (debugText != null) {
+            debugText.setText("Hello");
+        }
+
     }
+
+
 }
